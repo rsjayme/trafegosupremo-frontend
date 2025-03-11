49cd5a2b5d6dc51dc534a6528a9fa826ff60af4c
Raphael Jayme <rsjayme2@gmail.com>
2025-03-11 02:19:56 -0300

    fixes

--- src/app/(authenticated)/marcas/[id]/editar/page.tsx
+++ src/app/(authenticated)/marcas/[id]/editar/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import { useParams } from 'next/navigation';
 import { EditBrandPage } from "@/components/brands/EditBrandPage";
 
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function EditarMarca({ eita }: any) {
-    return <EditBrandPage brandId={eita.id} />;
+export default function EditarMarca() {
+    const params = useParams();
+
+
+    return <EditBrandPage brandId={params.id as string} />;
 }
