fff88f6a7e7831e615eaf7f030a46cc7d976b431
Raphael Jayme <rsjayme2@gmail.com>
2025-03-14 10:48:48 -0300

    fixxx

---
+++ remove-edit-page.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Remove a página de edição já que agora usamos modal
+rm -rf front/src/app/(authenticated)/nucleo-gestao/clients/[id]
+
+echo "Página de edição removida com sucesso!"
